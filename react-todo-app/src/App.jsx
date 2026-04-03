421c147bc86b5ddb677171d5f68f504759cf0799
Karishma <wagmi.nowww@gmail.com>
2026-04-03 11:21:22 +0530

    Initialized todos and input state

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,10 +1,13 @@
-
+import {useState} from "react";
 
 
 function App(){
+  const [input,setInput] = useState("");
+  const [todos,setTodos] = useState([]);
   return(
     <div>
       <h1> Todo App </h1>
+      <input value={input} onChange={(e)=>{setInput(e.target.value)}}/> 
     </div>
   )
 }
